d615a6e88100742985c6c312331a3fcc1dc502db
Snt <sant85082@gmail.com>
2026-08-05 18:19:53 -0500

    core: implementar estructura inicial del proyecto

---
+++ front/signia/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    // Interfaz de la aplicacion, por ahora vacia por ser la inicializacion!
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
